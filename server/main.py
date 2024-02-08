4abd8aa7d2cddc71a26495c90c3e634760142fe2
Francisco Caballero <fjcero@gmail.com>
2024-02-07 21:02:52 -0600

    feat: main features seem to work

--- server/main.py
+++ server/main.py
@@ -1,19 +1,30 @@
+import json
+import sys
+from contextlib import asynccontextmanager
 from typing import Annotated, Any
 from urllib.parse import urlencode
 
+import httpx
 from authlib.integrations.starlette_client import OAuth, OAuthError
-from fastapi import (Body, Depends, FastAPI, Form, HTTPException, Request, Security, requests,
-                     status)
+from fastapi import (Body, Depends, FastAPI, Form, HTTPException, Request,
+                     Security, requests, status)
 from fastapi.responses import RedirectResponse
 from fastapi.security import (HTTPBearer, OAuth2AuthorizationCodeBearer,
                               OAuth2PasswordBearer, OAuth2PasswordRequestForm)
 from fastapi_auth0 import Auth0, Auth0User
-import httpx
 from pydantic import BaseModel
 from starlette.middleware.sessions import SessionMiddleware
 
 from .config import get_settings
 
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     app.requests_client = httpx.AsyncClient()
+#     yield
+#     await app.requests_client.aclose()
+
+
+# app = FastAPI(lifespan=lifespan)
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="secret-key")
 
@@ -48,25 +59,28 @@ oauth.register(
     # jwks_uri=f'https://{settings.auth0_domain}/.well-known/jwks.json',
 )
 
+token_auth_scheme = HTTPBearer()
+
 # client = OAuth2Session(
 #     settings.auth0_client_id,
 #     settings.auth0_client_secret,
 #     scope="read:all write:all",
 # )
 
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-oauth2_scheme = OAuth2AuthorizationCodeBearer(
-    authorizationUrl=f"https://{settings.auth0_domain}/authorize",
-    tokenUrl=f"https://{settings.auth0_domain}/oauth/token",
-    refreshUrl=f"https://{settings.auth0_domain}/oauth/token",
-    scopes={
-        "openid": "auth with openid",
-        "email": "Access to the mail",
-        "profile": "access user information",
-        "read:all": "Read all contents",
-        "write:all": "Modify all contents",
-    },
-)
+# oauth2_scheme = OAuth2AuthorizationCodeBearer(tokenUrl="token", authorizationUrl="authorize")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2_scheme = OAuth2AuthorizationCodeBearer(
+#     authorizationUrl=f"https://{settings.auth0_domain}/authorize",
+#     tokenUrl=f"https://{settings.auth0_domain}/oauth/token",
+#     refreshUrl=f"https://{settings.auth0_domain}/oauth/token",
+#     scopes={
+#         "openid": "auth with openid",
+#         "email": "Access to the mail",
+#         "profile": "access user information",
+#         "read:all": "Read all contents",
+#         "write:all": "Modify all contents",
+#     },
+# )
 
 
 # origins = ["*"]
@@ -118,56 +132,53 @@ def root(request: Request):
 
 @app.get("/authorize")
 async def authorize(request: Request):
-    state = request.query_params.get('state')
-    scope = "read:all openid email profile read:all write:all offline_access"
-    redirect_uri = "https://eagle-major-notably.ngrok-free.app/authorize/callback"
-
-    # Construct the OAuth URL with the state parameter and an auth/callback url
-    params = {
-        "response_type": "code",
-        "client_id": settings.auth0_client_id,
-        "redirect_uri": redirect_uri,
-        "scope": scope,
-        "audience": settings.auth0_api_audience,
-        "access_type": "offline",
-        "prompt": "consent",
-        "state": state,
-    }
-    authorization_url = f"https://{settings.auth0_domain}/authorize"
-    return RedirectResponse(f"{authorization_url}?{urlencode(params)}")
-
-
-@app.get("/authorize/callback")
-async def authorize_callback(request: Request):
-    openai_redirect_uri = "https://chat.openai.com/aip/g-471ccbf64bbc25808e27b5bd1c897e7c529097a3/oauth/callback"
+    print("AUTHORIZE", request.query_params)
+    # openai_redirect_uri = request.query_params.get('redirect_uri')
     code = request.query_params.get('code')
     state = request.query_params.get('state')
-    # Redirect to OpenAI's callback URL with code and state
+    scope = "read:all write:all openid email profile offline_access"
+    redirect_uri = "https://eagle-major-notably.ngrok-free.app/authorize"
 
-    params = {"code": code, "state": state}
+    if code and state:
+        callback_params = {"code": code, "state": state}
+        openai_redirect_uri = "https://chat.openai.com/aip/g-248e136e506eb5cfd9b5bf6be172b6eda68557b4/oauth/callback"
+        callback_url = f"{openai_redirect_uri}?{urlencode(callback_params)}"
+        print("callback", callback_url)
 
-    redirect_uri = request.query_params.get('redirect_uri')
+        # async with httpx.AsyncClient() as client:
+        #     res = await client.post(
+        #         "https://eagle-major-notably.ngrok-free.app/token",
+        #         data={"code": code, "state": state},
+        #         # headers={"Content-Type": "application/json"},
+        #     )
 
-    print(f"Intermediate redirect with params = {params} and redirect = {redirect_uri}")
+        #     return json.loads(res.text)
 
-    # return RedirectResponse(f"{openai_redirect_uri}?{urlencode(params)}")
-    return RedirectResponse(f"/token?code={code}&state={state}")
+        return RedirectResponse(callback_url)
+    else:
+        # Construct the OAuth URL with the state parameter and an auth/callback url
+        params = {
+            "response_type": "code",
+            "client_id": settings.auth0_client_id,
+            "redirect_uri": redirect_uri,
+            "scope": scope,
+            "audience": settings.auth0_api_audience,
+            "access_type": "offline",
+            "prompt": "consent",
+            "state": state,
+        }
+        authorization_url = f"https://{settings.auth0_domain}/authorize"
+        return RedirectResponse(f"{authorization_url}?{urlencode(params)}")
 
 
-@app.get("/token")
-async def token(request: Request):
-    # print("TOKEN", request.query_params, request.headers, payload)
-    # return {
-    #     "access_token": "example_token",
-    #     "token_type": "bearer",
-    #     "refresh_token": "example_token",
-    #     "expires_in": 59,
-    # }
-    redirect_uri = "https://eagle-major-notably.ngrok-free.app/authorize/callback"
+@app.post("/token")
+async def token(request: Request, payload: Any = Body(None)):
+    redirect_uri = "https://eagle-major-notably.ngrok-free.app/authorize"
+    # print(request.url_for("/token"), request.url_for("/token"))
 
     try:
-        # request_data = await request.form()
-        params = request.query_params
+        params = await request.form()
+        print("TOKEN", request.query_params, request.headers, payload, params)
         code = params.get('code')
 
         print(f"token endpoint: request data = {params}")
@@ -177,19 +188,26 @@ async def token(request: Request):
             "code": code,
             "client_id": settings.auth0_client_id,
             "client_secret": settings.auth0_client_secret,
-            "redirect_uri": redirect_uri,  # Use the same redirect_uri as in /authorize
+            "redirect_uri": redirect_uri,
             "grant_type": "authorization_code",
         }
 
-        response = httpx.post(token_url, data=data)
+        async with httpx.AsyncClient() as client:
+            res = await client.post(
+                token_url,
+                data=data,
+            )
+
+            token_response = json.loads(res.text)
 
         # Check if the response from Google is successful
         # if response.status_code != 200:
         #     print("Error during token exchange with Google:", response.status_code, response.text)
         #     raise HTTPException(status_code=500, detail="Token exchange failed")
 
-        token_response = response.json()
-        print(token_response)
+        # token_response = {}
+        # response.json()
+        # print(token_response)
 
         # Check if the necessary tokens are present in the response
         # if "access_token" not in token_response or "refresh_token" not in token_response:
@@ -213,65 +231,10 @@ async def token(request: Request):
         raise HTTPException(status_code=500, detail="Unexpected error in token exchange")
 
 
-async def authtest(request: Request):
-    print("AUTHORIZE/CALLBACK", request.query_params)
-    # code=4GYQoHNv4Xr79Iw_7fxMGPHHIlq9iIvUJEX1PTmBWk_7E&state=123
-    token = get_token(
-        code=request.query_params['code'],
-        # redirect_uri="https://chat.openai.com/aip/g-471ccbf64bbc25808e27b5bd1c897e7c529097a3/oauth/callback"
-    )
-
-    # try:
-    #     access_token = await oauth.auth0.authorize_access_token(request)
-    # except OAuthError as error:
-    #     print("ERROR", error)
-    #     return RedirectResponse(url='/')
-
-    # user_data = await oauth.auth0.parse_id_token(access_token)
-    # payload = jwt.decode(token, settings., algorithms=[ALGORITHM])
-    print(token)
-
-    # request.session['user'] = {
-    #     "name": access_token['userinfo']['name'],
-    #     "email": access_token['userinfo']['email'],
-    # }
-    # request.session['auth'] = access_token['access_token']
-    # request.session['raw_auth'] = access_token
-
-    # print(request.session)
-    return {
-        "access_token": "example_token",
-        "token_type": "bearer",
-        "refresh_token": "example_token",
-        "expires_in": 59,
-    }
-
-    # return RedirectResponse(url=f'https://chat.openai.com/aip/g-471ccbf64bbc25808e27b5bd1c897e7c529097a3/oauth/callback?state={request.query_params.get('state')}')
-
-
 @app.get('/secure')
 # def secure_endpoint(user=Security(oauth2_scheme)):
 def secure_endpoint(user=Security(get_user)):
     return {"message": "This is a secure endpoint.", "user": user}
-
-
-# @app.post("/token")
-# async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-#     user_dict = fake_users_db.get(form_data.username)
-#     if not user_dict:
-#         raise HTTPException(
-#             status_code=400,
-#             detail="Incorrect username or password",
-#         )
-#     user = UserInDB(**user_dict)
-#     hashed_password = fake_hash_password(form_data.password)
-#     if not hashed_password == user.hashed_password:
-#         raise HTTPException(
-#             status_code=400,
-#             detail="Incorrect username or password"
-#         )
-
-#     return {"access_token": user.username, "token_type": "bearer"}
 
 
 # @app.get("/secure", dependencies=[Depends(auth.implicit_scheme)])
@@ -280,8 +243,8 @@ def secure_endpoint(user=Security(get_user)):
 
 
 @app.get("/query")
-def query(request: Request, payload: Any = Body(None)):
-    print("QUERY", request.query_params, request.headers, payload)
+def query(request: Request, token: str = Depends(token_auth_scheme)):
+    print("QUERY", token.credentials)
     return {
         "location": {"city": "New York", "state": "NY", "country": "USA"},
         "current_weather": {
@@ -308,36 +271,3 @@ def query(request: Request, payload: Any = Body(None)):
             ],
         },
     }
-
-
-async def get_authorize_url(
-    response_type: str,
-    redirect_uri: str,
-    scope: str,
-    state: str,
-):
-    query_string = urlencode(dict(
-        audience=settings.auth0_api_audience,
-        scope=scope,
-        response_type=response_type,
-        client_id=settings.auth0_client_id,
-        redirect_uri=redirect_uri,
-        state=state,
-    ))
-
-    url = f"https://{settings.auth0_domain}/authorize?{query_string}"
-
-    return url
-
-
-def get_token(code: str, redirect_uri: str | None = None):
-    url = f"https://{settings.auth0_domain}/oauth/token"
-
-    r = httpx.post(url=url, data=dict(
-        grant_type="authorization_code",
-        client_id=settings.auth0_client_id,
-        client_secret=settings.auth0_client_secret,
-        code=code,
-        redirect_uri="https://eagle-major-notably.ngrok-free.app/authorize/callback",
-    ))
-    print(r.json())
